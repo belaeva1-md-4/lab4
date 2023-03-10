701e0f46f56b50fa9f1b537e36d8371cfe208da4
belaeva1-md-4 <nastabelaeva2154@gmail.com>
2023-03-10 16:32:47 +0300

    цпу

--- 4 лаба/4l.py
+++ 4 лаба/4l.py
@@ -1,13 +1,26 @@
 
-
-def N(x):
+"eett"
+def s1():
+    print("Введите число")
+    x=int(input())
     if x%3==0:
-        return True
+        print("Делится")
     else:
-        return False
+        print("Не делится")
 
-c=int(input())
-if N(c):
-    print("Делится")
-else:
-    print("Не делится")
+
+def s2():
+    print("Введите число")
+    c=int(input())
+    try:
+        res= 100 / c
+        print(res)
+    except ValueError:
+        print("Вы ввели строку")
+    except ZeroDivisionError:
+        print("Деление на 0")
+
+s1(), s2()
+
+
+
